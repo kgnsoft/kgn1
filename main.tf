785ebad002b4144d0e82eb06310a201cddb28ea3
kgnsoft <bawchail@yahoo.com>
2019-04-22 01:08:48 +0100

    adding the details

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 
-provider = "aws" { 
-  region = "eu-west-2"
-}
+#provider = "aws" { 
+#  region = "eu-west-2"
+#}
 
-resources "aws_instances" "anis_dev_02" {
-  ami = "ami-7c1bfd1b"
-  instance_type = "t2.micro"
-  }
+
+resource "aws_instance" "anis" {
+  ami 		= "ami-7c1bfd1b"
+  instance_type	= "t2.micro"
+  
+  user_data =	<<-EOF
+		#!/bin/bash
+		echo "Hello, this is a firt instance" > index.html
+		nohup busybox httpd -f -p 8080 & 
+		EOF
+  tags {
+    Name = "anis_dev_02"
+}
 }
 
  
